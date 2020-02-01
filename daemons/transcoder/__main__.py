b1e2a5a1287a2eaf3fe8f10b142c14231eb8f2d9
bcskda <demulkup@ya.ru>
2020-02-01 13:53:21 +0300

    transcoder daemon: report finished jobs

--- daemons/transcoder/__main__.py
+++ daemons/transcoder/__main__.py
@@ -2,11 +2,14 @@ import signal
 import socketserver
 from config import Config
 from daemons.abc import JobQueueDaemon
-from .transcoder import TranscodeExecutor, TranscodeRequestHandler
+from .transcoder import TranscodeExecutor, TranscodeRequestHandler, ResultReporter
 
 
-def main(server_address):
-    daemon = JobQueueDaemon(TranscodeExecutor)
+def main(server_address, report_address):
+    daemon = JobQueueDaemon(["q_transcode_accept", "q_transcode_finished"])
+    report_queue = daemon.job_queues["q_transcode_finished"]
+    daemon.add_executor("q_transcode_accept", TranscodeExecutor, report_queue)
+    daemon.add_executor("q_transcode_finished", ResultReporter, report_address)
     daemon.add_server(socketserver.TCPServer(server_address, TranscodeRequestHandler))
     daemon.start()
     
@@ -21,4 +24,4 @@ def main(server_address):
 
 if __name__ == '__main__':
     Config.update("config.json")
-    main(('127.0.0.1', 1337))
+    main(('127.0.0.1', 1337), ('127.0.0.1', 1338))
--- daemons/transcoder/transcoder.py
+++ daemons/transcoder/transcoder.py
@@ -18,6 +18,8 @@ Output:
 {"error": 0, "result": {"accept": [0], "discard": []}}
 """
 
+import json
+import socket
 from typing import Dict
 from config import Config
 from transcode_v2 import transcode, TranscodeError, validate_args
@@ -39,7 +41,7 @@ class TranscodeRequestHandler(JsonRequestHandler):
 
         for idx, job in enumerate(self.request_obj):
             try:
-                self.server.daemon.job_queue.put(TranscodeJob(job))
+                self.server.daemon.job_queues["q_transcode_accept"].put(TranscodeJob(job))
                 self.response_obj["result"]["accept"].append(idx)
             except TranscodeError as e:
                 response["result"]["discard"].append({
@@ -60,7 +62,32 @@ class TranscodeJob:
         validate_args(self.inputs, self.outputs, self.profile)
 
 class TranscodeExecutor(BaseQueueExecutor):
+    def __init__(self, job_queue, report_queue):
+        super(TranscodeExecutor, self).__init__(job_queue)
+        self.report_queue = report_queue
+
     def handle_job(self, job):
         with open(f"{job.outputs[0]}.transcode_log", "w") as stderr:
             transcode(job.profile, job.inputs, job.outputs, stderr=stderr)
             # TODO report progress
+            self.report_queue.put(job.outputs)
+
+class ResultReporter(BaseQueueExecutor):
+    def __init__(self, job_queue, report_addr):
+        super(ResultReporter, self).__init__(job_queue)
+        self.report_addr = report_addr
+
+    def handle_job(self, job):
+        print(f"ResultReporter: finished {job}")
+        message = {
+            "message_type": "transcode_result",
+            "message": {"outputs": job}
+        }
+        with socket.create_connection(self.report_addr) as sock:
+            with sock.makefile("w") as sock_w:
+                json.dump(message, sock_w)
+            sock.shutdown(socket.SHUT_WR)
+            with sock.makefile("r") as sock_r:
+                ans = json.load(sock_r)
+            sock.shutdown(socket.SHUT_RD)
+            print(f"ResultReporter: remote answer: {ans}")
