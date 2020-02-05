13bf17d4bd800b145c042f64c6c36b35abace1c7
bcskda <demulkup@ya.ru>
2020-02-05 11:01:53 +0300

    [wip] count mountpoint references

--- daemons/importer/importer.py
+++ daemons/importer/importer.py
@@ -22,22 +22,36 @@ class ImportRequestHandler(daemons.abc.DispatchedRequestHandler):
             self.server.daemon.job_queues["q_upload"].put(path)
 
 class ImportExecutor(daemons.abc.BaseQueueExecutor):
-    def __init__(self, job_queue, output_dir, transcoder_address):
+    def __init__(self, job_queue, output_dir, transcoder_address, daemon):
         super(ImportExecutor, self).__init__(job_queue)
         self.output_dir = output_dir
         self.transcoder_addr = transcoder_address
+        self.active_imports = daemon.active_imports
+        self.active_transcodes = daemon.active_transcodes
 
     def handle_job(self, sd_root: str):
         concat_tasks = into_tasks(sd_root, self.output_dir)
+        if sd_root in self.active_imports:
+            raise KeyError(f"import already in progress: from={sd_root}")
+        self.active_imports[sd_root] = 0
         transcode_request = []
         for task in concat_tasks:
+            old_from = self.active_transcodes.get(task.destination)
+            if old_from:
+                print("transcode already in progress: from={} old_from={} output_file={}".format(
+                    sd_root, old_from, task.destination))
+                continue
+            self.active_transcodes[task.destination] = sd_root
+            self.active_imports[sd_root] += 1
             os.makedirs(os.path.dirname(task.destination), exist_ok=True)
             transcode_request.append({
                 "inputs": [[f.path for f in task.sources]],
                 "outputs": [task.destination],
                 "profile": Config.ff_default_profile
             })
+        self._send_transcode_request(transcode_request)
 
+    def _send_transcode_request(self, transcode_request):
         with socket.create_connection(self.transcoder_addr) as sock:
             with sock.makefile(mode="w") as sock_w:
                 json.dump(transcode_request, sock_w)
@@ -50,11 +64,14 @@ class ImportExecutor(daemons.abc.BaseQueueExecutor):
                     print(f"Unhandled exception: {e}")
 
 class UploadExecutor(daemons.abc.BaseQueueExecutor):
-    def __init__(self, job_queue, gdrive_client, remote_root_id, sources_root):
+    def __init__(self, job_queue, gdrive_client, remote_root_id, sources_root, report_addr, daemon):
         super(UploadExecutor, self).__init__(job_queue)
         self.gdrive_client = gdrive_client
         self.remote_root_id = remote_root_id
         self.sources_root = sources_root
+        self.report_addr = report_addr
+        self.active_imports = daemon.active_imports
+        self.active_transcodes = daemon.active_transcodes
 
     def handle_job(self, local_path: str):
         local_dir = os.path.dirname(local_path)
@@ -67,3 +84,26 @@ class UploadExecutor(daemons.abc.BaseQueueExecutor):
             on_each=lambda x: print(f"on_each {x}"),
             on_progress=lambda x: print(f"on_progress {x}")
         )
+        self._unregister_transcode_job(local_path)
+
+    def _unregister_transcode_job(self, local_path):
+        sd_root = self.active_transcodes.pop(local_path)
+        if self.active_imports[sd_root] == 1:
+            del self.active_imports[sd_root]
+            self._report_import_finish(sd_root)
+        else:
+            self.active_imports[sd_root] -= 1
+    
+    def _report_import_finish(self, sd_root):
+        message = {
+            "message_type": "import_finish",
+            "message": {"path": sd_root}
+        }
+        with socket.create_connection(self.report_addr) as sock:
+            with sock.makefile("w") as sock_w:
+                json.dump(message, sock_w)
+            sock.shutdown(socket.SHUT_WR)
+            with sock.makefile("r") as sock_r:
+                ans = json.load(sock_r)
+            sock.shutdown(socket.SHUT_RD)
+            print(f"Devwatch response: {ans}")
