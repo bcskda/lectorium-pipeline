b703723a8e39b7d04f4a7d10c25b063f0d0b5125
Dmitrii <demulkup@ya.ru>
2020-02-22 10:32:58 +0300

    Refactor

--- daemon2/pipeline_common/models.py
+++ daemon2/pipeline_common/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Create your models here.
 
 class ExportProto(models.Model):
     proto_nm = models.CharField(max_length=20)
+
 
 class ExportDestination(models.Model):
     export_proto = models.ForeignKey(ExportProto, on_delete=models.CASCADE)
     destination_nm = models.CharField(max_length=50)
     destination_params_txt = models.CharField(max_length=200)
+
 
 class BaseRequest(models.Model):
     start_dttm = models.DateTimeField()
@@ -16,24 +17,30 @@ class BaseRequest(models.Model):
     is_approved = models.BooleanField(default=False)
     is_finish_confirmed = models.BooleanField(default=False)
 
+
 class ImportRequest(BaseRequest):
     sdroot_path_txt = models.CharField(max_length=200)
 
+
 class TranscodeProfile(models.Model):
     profile_nm = models.CharField(max_length=20)
+
 
 class TranscodeRequest(BaseRequest):
     import_request = models.ForeignKey(ImportRequest, null=True, on_delete=models.SET_NULL)
     transcode_profile = models.ForeignKey(TranscodeProfile, null=True, on_delete=models.SET_NULL)
 
+
 class MediaFileRole(models.Model):
     role_nm = models.CharField(max_length=20)
+
 
 class MediaFile(models.Model):
     transcode_request = models.ForeignKey(TranscodeRequest, null=True, on_delete=models.SET_NULL)
     role = models.ForeignKey(MediaFileRole, null=True, on_delete=models.SET_NULL)
     path_txt = models.CharField(max_length=200)
     order_n = models.IntegerField()
+
 
 class ExportRequest(BaseRequest):
     media_file = models.ForeignKey(MediaFile, null=True, on_delete=models.SET_NULL)
---
+++ daemon2/pipeline_panel/models.py
@@ -0,0 +1,1 @@
+from daemon2.pipeline_common.models import *
